247f37cd5da2586a66a08e2010f548b101a16675
ivan <ivanov93@vk.com>
2023-05-09 13:40:04 +0300

    Creating a project

---
+++ src/main/java/ru/skypro/sockwarehouse/SockWarehouseApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.sockwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockWarehouseApplication.class, args);
+	}
+
+}
